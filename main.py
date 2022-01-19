9a2ea5a82a7d6b167c05bc4dce2b80530bff91be
foreverbeyoung <1370308287@qq.com>
2022-01-19 16:50:34 +0800

    v1.0

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 # -*- coding:utf-8 -*-
+
 import os
+from math import ceil
+
 import xlsxwriter
 import requests
 from openpyxl import Workbook,load_workbook
 
 from openpyxl.drawing.image import Image
+
 from hashlib import md5
 import pandas as pd
 from openpyxl.utils import get_column_letter, column_index_from_string
 import openpyxl
-IMG_COL = 'A'
+from PIL import JpegImagePlugin
+JpegImagePlugin._getmp = lambda  x:None
+
+excel_dir = 'demo.xlsx'
+img_org_save = r'F:\Amazon\看这里\test_imgs'
 
 def seed_get():
     # return os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, "demo.xlsx"))
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), "demo.xlsx"))
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), excel_dir))
 
 def file_name_encrypt(url):
     md5_inst = md5()
     md5_inst.update(url.encode())
+    print(url, md5_inst.hexdigest())
     return md5_inst.hexdigest()
 
 def save_org_img(url,filedir):
@@ -27,26 +36,34 @@ def save_org_img(url,filedir):
     file_name = file_name_encrypt(url)
     img_fp= res.content
     with open(os.path.join(filedir,file_name)+'.jpeg', 'wb') as f:
-        print(os.path.join(filedir,file_name))
+        # print(os.path.join(filedir,file_name))
          # for data in res.iter_content(128):
          #    f.write(data)
         f.write(img_fp)
+    f.close()
     # yield dir_position+file_name
     return os.path.join(filedir,file_name)+'.jpeg'
 
 def img_position_join(x,y):
     if isinstance(y,int):
         y = get_column_letter(y)
+
     return y+str(x)
 
 
 # def insert_img(sh, line, filedir,img_positon):
-def insert_img(sh, filedir,img_positon):
+def insert_img(sh,line, filedir,img_positon):
     print(img_positon)
-    # sh.row_dimensions[line].height=50
+    sh.row_dimensions[line].height=50
     img = Image(filedir)
-    print(img.width,img.height,img.format)
-    img.width, img.height=img.width/8,img.height/8
+    print(img.width,img.height,img.format) #format 图片格式 quality图片质量
+    print(filedir)
+    img.format='jpeg'
+    img.width = ceil(img.width * 55/ img.height)
+    img.height = 55
+    print(img.width,img.height)
+
+
     sh.add_image(img, img_positon)
 
 def pd_read_excel():
@@ -80,14 +97,16 @@ if __name__ == "__main__":
     print(ws.max_column)
     ws.insert_cols(2,1)
     ws['B1']='img_exhibit'
+    ws.column_dimensions['B'].width = 10
 
 #     for fv in ws[2:ws.max_row]:
     for fv in ws.iter_rows(min_row=2,min_col=1):
-        print(fv[1].value)
-        img_dir = save_org_img(fv[2].value, r'test_img')
+
+        img_dir = save_org_img(fv[2].value, img_org_save)
         print(img_dir)
-        insert_img(ws,img_dir,img_position_join(fv[2].row,str(1)))
-    wb.save('demo.xlsx')
+        # input()
+        insert_img(ws,fv[2].row,img_dir,img_position_join(fv[2].row,2))
+    wb.save(excel_dir)
 
 #         # for sv in fv:
 # #         #     print(sv.value)
